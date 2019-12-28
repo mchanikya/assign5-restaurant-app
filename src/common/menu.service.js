71c6789f2cbc9897f7ccc22ff3c520e4f7c70f18
chanikya <chanikya.prakash@gmail.com>
2019-12-28 13:17:39 +0530

    added validation of fav and showing error msg if fav not found

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -16,15 +16,12 @@ function MenuService($http, ApiPath) {
   };
 
 
-  service.getMenuItems = function (category) {
-    var config = {};
-    if (category) {
-      config.params = {'category': category};
-    }
-
-    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
-      return response.data;
-    });
+  service.getMenuItems = function (shortName) {
+    var response = $http({
+          method: "GET",
+          url: (ApiPath + 'menu_items/'+shortName+'.json'),
+        });
+    return response;
   };
 
 }
--- src/public/userSignUp/user-signup-controller.js
+++ src/public/userSignUp/user-signup-controller.js
@@ -4,8 +4,8 @@
   angular.module('public')
   .controller('UserSignUpController',UserSignUpController);
 
-  UserSignUpController.$inject = ['userInfoService'];
-  function UserSignUpController(userInfoService) {
+  UserSignUpController.$inject = ['userInfoService','MenuService'];
+  function UserSignUpController(userInfoService,MenuService) {
     var $ctrl = this;
     $ctrl.user={};
     $ctrl.user.firstName="";
@@ -13,13 +13,26 @@
     $ctrl.user.eMail='';
     $ctrl.user.pNumber='';
     $ctrl.user.favDish='';
+    $ctrl.favFound=false;
     $ctrl.validateUser=function(){
       console.log($ctrl.user.firstName);
       console.log($ctrl.user.lastName);
       console.log($ctrl.user.eMail);
       console.log($ctrl.user.pNumber);
       console.log($ctrl.user.favDish);
-      userInfoService.storeUser($ctrl.user);
+      var response = MenuService.getMenuItems($ctrl.user.favDish);
+      console.log("data",response);
+      response.then(function(data){
+        $ctrl.favFound=false;
+        console.log("short_Name",data.data.short_name);
+        userInfoService.storeUser($ctrl.user);
+      }).catch(function(error){
+        console.log('Failed to fetch details');
+        $ctrl.favFound=true;
+      });
+      // console.log("data.status",data.$$state.status);
+      // console.log("data.value",data.$$state['status']);
+      // if
     };
   }
 
