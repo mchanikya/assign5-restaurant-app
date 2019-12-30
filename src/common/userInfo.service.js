a17f9bc847e9f1b6dc24323041a1daeebcd37dce
Chanikya <chanikyaprakash@gmail.com>
2019-12-30 18:31:08 +0530

    validations and added image in myinfo page

--- src/common/userInfo.service.js
+++ src/common/userInfo.service.js
@@ -2,22 +2,31 @@
 	'use strict';
 
 	angular.module('common')
-	.service('userInfoService',userInfoService);
+	.service('userInfoService',userInfoService)
+	.filter('splitStringAndNumber', [ '$filter' ,function($filter){
+	    return function(string){
+	      var matches = string.match(/\d+|[a-z]+/ig);
+	      return matches[0];
+	    };
+	}]);
 
 	function userInfoService(){
 		var service=this;
 		service.userDetails=[];
 		service.getUserInfo=function(){
-			return service.userDetails[0];
+			return service.userDetails[service.userDetails.length-1];
 		};
 		service.storeUser=function(user){
 			service.userDetails.push(user);
-			console.log("In Service",user.firstName);
-			console.log("In Service",user.lastName);
-			console.log("In Service",user.eMail);
-			console.log("In Service",user.pNumber);
-			console.log("In Service",user.favDish);
+			// console.log("In Service",user.firstName);
+			// console.log("In Service",user.lastName);
+			// console.log("In Service",user.eMail);
+			// console.log("In Service",user.pNumber);
+			// console.log("In Service",user.favDish);
 		};
 	}
+
+
+
 
 })();
--- src/public/userSignUp/user-signup-controller.js
+++ src/public/userSignUp/user-signup-controller.js
@@ -13,22 +13,24 @@
     $ctrl.user.eMail='';
     $ctrl.user.pNumber='';
     $ctrl.user.favDish='';
+    $ctrl.user.favItem={};
     $ctrl.favFound=false;
+    $ctrl.infoFlag=false;
+
     $ctrl.validateUser=function(){
-      console.log($ctrl.user.firstName);
-      console.log($ctrl.user.lastName);
-      console.log($ctrl.user.eMail);
-      console.log($ctrl.user.pNumber);
-      console.log($ctrl.user.favDish);
       var response = MenuService.getMenuItems($ctrl.user.favDish);
       console.log("data",response);
       response.then(function(data){
         $ctrl.favFound=false;
+        console.log("item details",data.data);
         console.log("short_Name",data.data.short_name);
         userInfoService.storeUser($ctrl.user);
+        $ctrl.user.favItem=data.data;
+        $ctrl.infoFlag=true;
       }).catch(function(error){
         console.log('Failed to fetch details');
         $ctrl.favFound=true;
+        $ctrl.infoFlag=false;
       });
       // console.log("data.status",data.$$state.status);
       // console.log("data.value",data.$$state['status']);
